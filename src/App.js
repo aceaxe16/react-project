03c73891224c9c1b90450d9766c4aa4a21232071
aceaxe16 <branimir88@abv.bg>
2023-03-27 19:38:01 +0300

    controlled login form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Header } from './components/Header/Header';
-import { Home } from './components/HomePage/Home';
-import { Login } from './components/Login/Login';
-import { Register } from './components/Register/Register';
-import { Catalog } from './components/Catalog/Catalog';
-import { Profile } from './components/Profile/Profile';
-import { AddManga } from './components/AddManga/AddManga';
+import { AuthContext } from "./contexts/AuthContext";
+
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/HomePage/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { Catalog } from "./components/Catalog/Catalog";
+import { Profile } from "./components/Profile/Profile";
+import { AddManga } from "./components/AddManga/AddManga";
+import { useState } from "react";
+
 
 function App() {
+  const [auth, setAuth] = useState({});
+
+  const onLoginSubmit = async (data) => {
+    
+    console.log(data);
+  };
+
   return (
-    <div>
-      <Header/>
-      <Routes>
-        <Route path = "/home" element = {<Home/>}/>
-        <Route path = "/add-manga" element = {<AddManga/>}/>
-        <Route path = "/login" element = {<Login/>}/>
-        <Route path = "/register" element = {<Register/>}/>
-        <Route path = "/services" element = {<Catalog/>}/>
-        <Route path = "/profile" element = {<Profile/>}/>
-      </Routes>
-      
-    </div>
+    <AuthContext.Provider value = {{onLoginSubmit}}>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/add-manga" element={<AddManga />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/services" element={<Catalog />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,48 @@
+import { Link } from "react-router-dom";
+
 export const Register = () => {
     return (
-        <p>Register page</p>
+        <section id="register-page" className="content auth">
+            <form id="register" method="post" >
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Register</h1>
+
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="maria@email.com"
+                        //value={values.email}
+                        //onChange={changeHandler}
+                    />
+
+                    <label htmlFor="pass">Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="register-password"
+                        //value={values.password}
+                        //onChange={changeHandler}
+                    />
+
+                    <label htmlFor="con-pass">Confirm Password:</label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        id="confirm-password"
+                        //value={values.confirmPassword}
+                        //onChange={changeHandler}
+                    />
+
+                    <input className="btn submit" type="submit" value="Register" />
+
+                    <p className="field">
+                        <span>If you already have profile click <Link to="/login">here</Link></span>
+                    </p>
+                </div>
+            </form>
+        </section>
     )
 }
